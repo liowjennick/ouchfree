aa42798ae6a07210dbb22c54a31637468ac7b30d
Liow Jen Nick <jennick@compass-interactive.com>
2019-08-16 23:38:35 +0800

    Add progress slider.

---
+++ src/js/choose-a-plan.js
@@ -0,0 +1,15 @@
+window.addEventListener('DOMContentLoaded', () => {
+    var app = new Vue({
+        el: '#choose-a-plan-container',
+        data () {
+            return {
+                step: 1
+            }
+        },
+        methods: {
+            submitForm () {
+                // some function here
+            }
+        }
+    })
+})
