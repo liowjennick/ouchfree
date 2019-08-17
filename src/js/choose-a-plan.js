e7b1c2fa2829fa5d91f69155ce5e1ab5b1dd2be3
Liow Jen Nick <jennick@compass-interactive.com>
2019-08-17 10:20:32 +0800

    Add form.

--- src/js/choose-a-plan.js
+++ src/js/choose-a-plan.js
@@ -1,9 +1,14 @@
 window.addEventListener('DOMContentLoaded', () => {
     var app = new Vue({
         el: '#choose-a-plan-container',
+        name: 'choose-a-plan',
+        components: {
+            vuejsDatepicker
+        },
         data () {
             return {
-                step: 1
+                step: 1,
+                dateOfBirth: new Date(2016, 9, 16)
             }
         },
         methods: {
